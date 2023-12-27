0a8afc45bbcac266912a809f946a1861cecb409e
teddychuks <chukwukaohuruogu@gmail.com>
2023-12-27 07:47:52 +0100

    main

--- src/features/charts/MenuStats.jsx
+++ src/features/charts/MenuStats.jsx
@@ -2,71 +2,104 @@ import ReactApexChart from "react-apexcharts";
 import { useMenuStats } from "../../hooks/useCharts";
 import Spinner from "../../ui/Spinner";
 
-const MenuStats = () => {
+const Chart = () => {
   const { isFetching, menuStatsDataInfo } = useMenuStats();
 
   if (isFetching) {
     return <Spinner />;
   }
 
-  if (
-    !menuStatsDataInfo ||
-    !menuStatsDataInfo.data ||
-    !menuStatsDataInfo.data.statistics.length
-  ) {
-    return null;
-  }
+  const newData = menuStatsDataInfo?.data?.statistics;
 
-  const categories = menuStatsDataInfo.data.statistics.map((item) => item._id);
-  const series = [
-    {
-      name: "Number of Ratings",
-      data: menuStatsDataInfo.data.statistics.map((item) => item.numRating),
-    },
-    {
-      name: "Minimum Price",
-      data: menuStatsDataInfo.data.statistics.map((item) => item.minPrice),
-    },
-    {
-      name: "Maximum Price",
-      data: menuStatsDataInfo.data.statistics.map((item) => item.maxPrice),
-    },
-    {
-      name: "Average Rating",
-      data: menuStatsDataInfo.data.statistics.map((item) => item.avgRating),
-    },
-    {
-      name: "Average Price",
-      data: menuStatsDataInfo.data.statistics.map((item) => item.avgPrice),
-    },
-  ];
-
-  const updatedOptions = {
-    chart: {
-      height: 350,
-      type: "bar",
-      toolbar: { show: false },
-    },
-    plotOptions: {
-      bar: { horizontal: false, columnWidth: "55%", endingShape: "rounded" },
-    },
-    dataLabels: { enabled: false },
-    stroke: { show: true, width: 2, colors: ["transparent"] },
-    xaxis: {
-      categories: categories,
-      labels: { style: { fontSize: "15px" } },
-    },
-    yaxis: {
-      title: { text: "Values", style: { fontSize: "16px" } },
-      labels: { style: { fontSize: "14px" } },
-      logarithmic: true,
-      min: 0.3,
-    },
-    fill: { opacity: 1 },
-    tooltip: {
-      y: {
-        formatter: function (val) {
-          return val;
+  const chartData = {
+    series: [
+      {
+        name: "Number of Ratings",
+        type: "column",
+        data: newData.map((item) => item.numRating),
+      },
+      {
+        name: "Minimum Price",
+        type: "area",
+        data: newData.map((item) => item.minPrice),
+      },
+      {
+        name: "Maximum Price",
+        type: "line",
+        data: newData.map((item) => item.maxPrice),
+      },
+      {
+        name: "Average Rating",
+        type: "line",
+        data: newData.map((item) => item.avgRating),
+      },
+      {
+        name: "Average Price",
+        type: "line",
+        data: newData.map((item) => item.avgPrice),
+      },
+    ],
+    options: {
+      chart: {
+        height: 350,
+        type: "line",
+        stacked: false,
+        toolbar: {
+          autoSelected: "zoom",
+          tools: {
+            download: false,
+            selection: true,
+            zoom: true,
+            zoomin: true,
+            zoomout: true,
+            pan: true,
+            reset: true,
+          },
+        },
+      },
+      stroke: {
+        width: [0, 2, 5],
+        curve: "smooth",
+      },
+      plotOptions: {
+        bar: {
+          columnWidth: "50%",
+        },
+      },
+      fill: {
+        opacity: [0.85, 0.25, 1],
+        gradient: {
+          inverseColors: false,
+          shade: "light",
+          type: "vertical",
+          opacityFrom: 0.85,
+          opacityTo: 0.55,
+          stops: [0, 100, 100, 100],
+        },
+      },
+      labels: newData.map((item) => item._id),
+      markers: {
+        size: 0,
+      },
+      xaxis: {
+        type: "category",
+      },
+      yaxis: {
+        title: {
+          text: "Values",
+        },
+        min: 0,
+      },
+      tooltip: {
+        shared: true,
+        intersect: false,
+        y: {
+          formatter: function (y) {
+            if (typeof y !== "undefined") {
+              return y.toFixed(2);
+            }
+            return y;
+          },
         },
       },
     },
@@ -74,17 +107,17 @@ const MenuStats = () => {
 
   return (
     <div>
-      <h2 className="flex font-bold">Menu Statistics</h2>
-      <div id="chart" className="">
+      <h2 className="px-2 flex font-bold">Menu Statistics</h2>
+      <div id="chart">
         <ReactApexChart
-          options={updatedOptions}
-          series={series}
-          type="bar"
-          height={287}
+          options={chartData.options}
+          series={chartData.series}
+          type="line"
+          height={350}
         />
       </div>
     </div>
   );
 };
 
-export default MenuStats;
+export default Chart;
--- src/hooks/useCharts.js
+++ src/hooks/useCharts.js
@@ -2,16 +2,12 @@ import { useQuery } from "@tanstack/react-query";
 import { getAggregation, getMenuStats } from "../services/apiChart";
 
 export function useMenuStats() {
-  const {
-    isFetching,
-    data: menuStatsDataInfo,
-    isError,
-  } = useQuery({
+  const { isFetching, data: menuStatsDataInfo } = useQuery({
     queryKey: ["menustats_data"],
     queryFn: () => getMenuStats(),
   });
 
-  return { isFetching, menuStatsDataInfo, isError };
+  return { isFetching, menuStatsDataInfo };
 }
 
 export function useAggregation() {
